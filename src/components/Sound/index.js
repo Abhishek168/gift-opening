1c01b7d6f42d27a01355987852ff46c9104278ff
Abhishek4298 <abhishek.bhavsar@bacancy.com>
2022-10-18 22:33:50 +0530

    final commit

--- src/components/Sound/index.js
+++ src/components/Sound/index.js
@@ -20,7 +20,6 @@ const Music = () => {
 							<div className='d-flex justify-content-center align-items-center'>
 								<img src={photo2} height='430px' alt='audio immg' />
 								<div>
-									Hello
 									<img
 										src={
 											'https://ia802905.us.archive.org/8/items/clickme_202004/tapme.gif'
